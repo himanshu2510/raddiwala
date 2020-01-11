7ca28797610d8bf99fece139936bd072e1d1b99b
naman03 <naman.ruhela3@gmail.com>
2020-01-11 15:06:06 +0530

    new exception file

---
+++ src/main/java/com/raddiwala/web/exceptions/InvalidInputException.java
@@ -0,0 +1,7 @@
+package com.raddiwala.web.exceptions;
+
+public class InvalidInputException extends Throwable {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
