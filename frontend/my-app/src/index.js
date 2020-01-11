4128c2a35e92e8282878b8c64e2b27f32e15d06f
leo96tush <41864567+leo96tush@users.noreply.github.com>
2020-01-11 12:58:17 +0530

    new login form

--- frontend/my-app/src/index.js
+++ frontend/my-app/src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Container, Row, Col, Jumbotron, Card, CardBody } from "reactstrap";
+import LoginForm from "./components/loginForm.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <Row>
+          <Col />
+          <Col lg="8">
+            <Jumbotron>
+              <h3>
+                Raddiwala.com
+              </h3>
+              <hr />
+              <Card>
+                <CardBody>
+                  <LoginForm />
+                </CardBody>
+              </Card>
+            </Jumbotron>
+          </Col>
+          <Col />
+        </Row>
+      </Container>
+    </div>
+  );
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
